ef2ef85d84abb291a81c065bdd6216ba26154e6c
garvey moore <moore.garvey@gmail.com>
2022-03-10 11:58:46 -0500

    app.set was abstantiatied

--- app.js
+++ app.js
@@ -1,14 +1,45 @@
-const http = require('http');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+// 💡 Add express session -- this helps us manage state
+var session = require('express-session');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+var indexRouter = require('./routes/index');
+var app = express();
+// 💡 Initialize express session
+app.use(session({ 
+  secret: 'csbot is best', 
+  resave: false, 
+  saveUninitialized: false 
+}));
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
+
+module.exports = app;
